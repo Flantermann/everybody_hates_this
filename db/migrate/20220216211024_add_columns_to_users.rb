7fbc58ae11894278e4d8e87ebca0f467479a4bd4
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-02-16 22:35:36 +0100

    migration adding columns to user

---
+++ db/migrate/20220216211024_add_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :nickname, :string
+    add_column :users, :age, :integer
+    add_column :users, :address, :string
+    add_column :users, :languages, :integer
+  end
+end
