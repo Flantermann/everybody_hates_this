31c3bba49afc08e043eb0cbf719456a8c2d2154b
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-02-16 23:03:23 +0100

    start with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :issues, dependent: :destroy
+  has_one_attached :photo
+  enum languages: { german: 0, englisch: 1, both: 2}
+
+  validates :email, :first_name, :last_name, :nickname, :age, :address, :languages, presence: true
+  validates :email, presence: true, uniqueness: true, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  }
 end
