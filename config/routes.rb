443810d37c2be42ecd4b931e47091ac49601a1c3
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-02-17 20:07:46 +0100

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :issues do
-    resources :milestones, only: [ :create, :update, :destroy ]
+    resources :milestones, only: :create
   end
+  resources :milestones, only: [ :update, :destroy ]
 end
