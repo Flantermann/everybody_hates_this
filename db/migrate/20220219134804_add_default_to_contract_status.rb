73451057bfc5f41f729ade1d83a813fbcbb76e23
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-02-19 16:59:07 +0100

    change migration status

--- db/migrate/20220219134804_add_default_to_contract_status.rb
+++ db/migrate/20220219134804_add_default_to_contract_status.rb
@@ -1,6 +1,6 @@
 class AddDefaultToContractStatus < ActiveRecord::Migration[6.1]
   def change
     remove_column :contracts, :status
-    add_column :contracts, :status, default: "pending"
+    add_column :contracts, :status, default: 0
   end
 end
