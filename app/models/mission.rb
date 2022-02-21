20ab5fbfe6f94c9790f6bbb48f918c18cf2407f4
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-02-21 11:00:30 +0100

    changed mission category and prefered buddy location

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,9 +3,9 @@ class Mission < ApplicationRecord
   belongs_to :contract, optional: true
   has_many :milestones
 
-  enum category: { body_mind: 0, health: 1, administration: 2, relationship: 3, work: 4, household: 5 }
+  enum category: { misc: 0, body_mind: 1, administration: 2, relationship: 3, work: 4, household: 5 }
   enum timeframe: { seven_d: 0, two_w: 1, four_w: 2, six_w: 3, nW: 4 }
-  enum prefered_buddy_location: { near: 0, far: 1, too_far: 2 }
+  enum prefered_buddy_location: { near: 0, anywhere: 1 }
 
   validates :description, presence: true, length: { minimum: 50 }
   validates :title, :category, :timeframe, :prefered_buddy_location, presence: true
