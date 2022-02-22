b9d1fc050d7ca6edf956c2993945a13efa2ff58e
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-02-22 19:32:57 +0100

    implemented edit, destroy and update methods

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,4 +1,5 @@
 class MissionsController < ApplicationController
+  before_action :set_mission, only: [:show, :edit, :update, :destroy]
   def index
     @missions = policy_scope(Mission)
   end
@@ -20,22 +21,34 @@ class MissionsController < ApplicationController
   end
 
   def show
-    @mission = Mission.find(params[:id])
-    authorize @mission
   end
 
   def edit
   end
 
   def update
+    if @mission.update(mission_params)
+      redirect_to @mission, notice: "Your mission was successfully updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @mission.destroy
+    # redirect_to root_path, notice: "Your mission was successfully deleted"
+    # there should be some logic that your buddy gets a notice when you destroy your mission.
+    # destroying a mission should also destroy the contract
   end
 
   private
 
   def mission_params
     params.require(:mission).permit(:category, :timeframe, :title, :description, :prefered_buddy_age_start, :prefered_buddy_age_end, :prefered_buddy_location )
+  end
+
+  def set_mission
+    @mission = Mission.find(params[:id])
+    authorize @mission
   end
 end
