6a8436d0a426f5d8a121f2290c79479c88fd888d
marco fox <marco.a.fox@gmail.com>
2022-02-24 21:06:28 +0100

    Created Controller for Chatrooms & Messages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
   resources :missions do
     resources :milestones, only: :create
   end
