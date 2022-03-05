52c0ec689d36e36ef6b4449982aad228730e3ab0
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-03-05 10:19:48 +0100

    milestones controller, chnage in contract model

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,12 +1,7 @@
 class Contract < ApplicationRecord
   # das sind neuen Associations, contract hat jetzt zwei user_ids, als asker und als receiver. optional: true braucht man auch
-  belongs_to :asker, class_name: :User, optional: true
-  belongs_to :receiver, class_name: :User, optional: true
-  # geht das hier mit dem "mission through users?"" braucht man das überhaupt?
+  belongs_to :asker, class_name: "User", optional: true # foreign_key: :asker_id
+  belongs_to :receiver, class_name: "User", optional: true # foreign_key: :receiver_id
 
-  # has_many :asker_missions, through: :asker
-  # has_many :receiver_missions, through: :receiver
-  # wenn man das oben benutzt, kann man die missions nur einzeln aufrufen
-  # Man müsste das aber zusammenführen können, zu allen missions, die ein contract hat
   enum status: { pending: 0, signed: 1, fullfilled: 2 }
 end
--- db/migrate/20220303091633_add_user_id_to_contracts.rb
+++ db/migrate/20220303091633_add_user_id_to_contracts.rb
@@ -3,6 +3,8 @@ class AddUserIdToContracts < ActiveRecord::Migration[6.1]
     change_table(:contracts) do |t|
       t.references :asker, foreign_key: { to_table: 'users' }
       t.references :receiver, foreign_key: { to_table: 'users' }
+      t.belongs_to :asker, foreign_key: { to_table: :users }
+      t.belongs_to :receiver, foreign_key: { to_table: :users }
     end
   end
 end
