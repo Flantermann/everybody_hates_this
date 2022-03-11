16120c5b69c078e5f175e50ff45fc7821be7a9f3
Franziska Lantermann <franziska.lantermann@gmail.com>
2022-03-11 15:09:57 +0100

    made filtering work again but result is one row per result, why?

--- app/javascript/channels/category.js
+++ app/javascript/channels/category.js
@@ -1,20 +1,20 @@
-import Rails from "@rails/ujs"
-const initCategoryForm = () => {
-    var checkBoxes = document.querySelectorAll(".form-check-input");
-    var form = document.getElementById('category-form');
+// import Rails from "@rails/ujs"
+// const initCategoryForm = () => {
+//     var checkBoxes = document.querySelectorAll(".form-check-input");
+//     var form = document.getElementById('category-form');
 
-    for (const check of checkBoxes) {
-    check.addEventListener( 'change', function(event) {
-        event.preventDefault()
-        if(this.checked) {
-          console.log("checked")
-          Rails.ajax({
-            url: `/missions?category=${check.value}`,
-            type: 'get',
-          });     
-        } else {
-          console.log("unchecked")
-        }
-    });
-    }
-}
+//     for (const check of checkBoxes) {
+//     check.addEventListener( 'change', function(event) {
+//         event.preventDefault()
+//         if(this.checked) {
+//           console.log("checked")
+//           Rails.ajax({
+//             url: `/missions?category=${check.value}`,
+//             type: 'get',
+//           });
+//         } else {
+//           console.log("unchecked")
+//         }
+//     });
+//     }
+// }
